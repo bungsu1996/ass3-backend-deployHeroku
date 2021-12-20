35d2407179005919deeca73a7e59e70f519e9ad0
bungsu1996 <venomous.zach@gmail.com>
2021-12-20 14:54:16 +0700

    deploy-update 6

--- dist/server.js
+++ dist/server.js
@@ -4,8 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var app_1 = __importDefault(require("./app"));
-var dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
 var Server = /** @class */ (function () {
     function Server() {
         this.app = app_1.default;
